7e09a261deb97f8b2f1088188e94a56bcd82797e
geecrypt <gavina352@gmail.com>
2021-10-13 12:16:33 -0700

    match TrucksController requirements to generated trucks_controller

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -1,23 +1,25 @@
 class TrucksController < ApplicationController
-  before_action :set_truck, only: %i[ show edit update destroy ]
+  before_action :set_truck, only: %i[show edit update destroy]
 
+  # viewing a list of trucks
   # GET /trucks or /trucks.json
   def index
     @trucks = Truck.all
   end
 
+  # viewing one single truck
   # GET /trucks/1 or /trucks/1.json
-  def show
-  end
+  def show; end
 
+  # creating a new truck (admin only)
   # GET /trucks/new
   def new
     @truck = Truck.new
   end
 
+  # editing an existing truck (admin only)
   # GET /trucks/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /trucks or /trucks.json
   def create
@@ -25,7 +27,7 @@ class TrucksController < ApplicationController
 
     respond_to do |format|
       if @truck.save
-        format.html { redirect_to @truck, notice: "Truck was successfully created." }
+        format.html { redirect_to @truck, notice: 'Truck was successfully created.' }
         format.json { render :show, status: :created, location: @truck }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -34,11 +36,12 @@ class TrucksController < ApplicationController
     end
   end
 
+  # reserve a truck (driver)
   # PATCH/PUT /trucks/1 or /trucks/1.json
   def update
     respond_to do |format|
       if @truck.update(truck_params)
-        format.html { redirect_to @truck, notice: "Truck was successfully updated." }
+        format.html { redirect_to @truck, notice: 'Truck was successfully updated.' }
         format.json { render :show, status: :ok, location: @truck }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -47,23 +50,25 @@ class TrucksController < ApplicationController
     end
   end
 
+  # deleting a truck (admin only)
   # DELETE /trucks/1 or /trucks/1.json
   def destroy
     @truck.destroy
     respond_to do |format|
-      format.html { redirect_to trucks_url, notice: "Truck was successfully destroyed." }
+      format.html { redirect_to trucks_url, notice: 'Truck was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_truck
-      @truck = Truck.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def truck_params
-      params.require(:truck).permit(:name, :company_id, :type, :year, :capacity, :reserved)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_truck
+    @truck = Truck.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def truck_params
+    params.require(:truck).permit(:name, :company_id, :type, :year, :capacity, :reserved)
+  end
 end
